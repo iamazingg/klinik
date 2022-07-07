2162e84e1db7a30cd9f233da75f816c648513690
iamazingg <hello@nyomandarmawan.id>
2022-07-07 13:14:09 +0800

    asjkdbakjsbd"

--- src/index.js
+++ src/index.js
@@ -5,19 +5,12 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { BrowserTracing } from '@sentry/tracing';
+
 import {
   BrowserRouter as Router,
   Routes,
 } from 'react-router-dom';
-Sentry.init({
-  dsn: 'https://1b45f1bb7294477e95a6186822df4cc7@o1308679.ingest.sentry.io/6554247',
-  integrations: [new BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
